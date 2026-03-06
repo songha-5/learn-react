e2e4c2eb23f6337c472e2b46d8351b0bdd86ed73
songha-5 <shn0518@gmail.com>
2026-03-06 15:13:51 +0900

    학습: css module 클래스 이름 충돌문제 해결

--- vite@latest/src/App.tsx
+++ vite@latest/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import style from './App.module.css'
 
 /**
  * JSX (JavaScript eXtension: 자바스크립트 확장 (비표준: 브라우저 해석 못함: SyntaxError))
@@ -8,24 +8,24 @@ import './App.css'
 function App() {
   return (
     <>
-      <header className='header'>
+      <header className={style.header}>
         <h1>
           <dfn><abbr>JSX</abbr></dfn> 기초 배우기
         </h1>
       </header>
-      <section>
+      <section className={style.main}>
         <h2><abbr>HTML</abbr>이 아닙니다</h2>
-        <label htmlFor='username'>이름</label>
-        <input id='username' type='text' className='input' placeholder='이름을 입력하세요.'/>
+        <label className={style.field} htmlFor='username'>이름</label>
+        <input id='username' type='text' className={style.input} placeholder='이름을 입력하세요.'/>
       </section>
       <section>
         <h2>웹 표준과 접근성을 준수해야 합니다.</h2>
         {/* 클릭 이벤트 리스너 추가 onClick = {함수} */}
         {/* button은 button으로! div X */}
         <div onClick={() => alert('no div')}>no div</div>
-        <button onClick={() => alert('yes button')}>사용자 고려버튼</button>
+        <button className={style.button} onClick={() => alert('yes button')}>사용자 고려버튼</button>
       </section>
-      <footer className='footer'>
+      <footer className={style.footer}>
         <small>
           COPYRIGHT RESERVE &copy; <abbr>카피라이터!</abbr>
         </small>
