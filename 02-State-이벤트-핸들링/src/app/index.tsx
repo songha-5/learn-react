9808861c9721115959a5ccc70a191e95004a0396
songha-5 <shn0518@gmail.com>
2026-03-09 14:25:06 +0900

    학습: 함수전달, 기본값 전달

--- 02-State-이벤트-핸들링/src/app/index.tsx
+++ 02-State-이벤트-핸들링/src/app/index.tsx
@@ -15,9 +15,15 @@ export default function App() {
       <Section title="타이틀입니다" isShowTitle={true}>
         <p>섹션입니다.</p>
         <div style={{ display: 'flex', gap: 6, marginTop: 12 }}>
-          <Button>Reaction</Button>
+          {/* if문 // 삼항연산자로 할때 */}
+          {/* <Button>Reaction</Button>
           <Button isDisabled>Reactive</Button>
-          <Button>Reactivity</Button>
+          <Button>Reactivity</Button> */}
+
+          {/* 함수전달방식 */}
+          <Button onNoti={() => alert('버튼!')}>Reaction</Button>
+          <Button onNoti={() => alert('버튼!')} isDisabled>Reactive</Button>
+          <Button onNoti={() => alert('버튼!')}>Reactivity</Button>
         </div>
       </Section>
       <Footer slogan={'완주 이후엔 스스로 학습이 가능!'}/>
--- 02-State-이벤트-핸들링/src/components/Button/index.tsx
+++ 02-State-이벤트-핸들링/src/components/Button/index.tsx
@@ -2,11 +2,14 @@ import type { ReactNode } from 'react'
 import S from './style.module.css'
 
 interface ButtonProps {
+  type?: 'submit' | 'button' | 'reset'
   children: ReactNode
   isDisabled?: boolean
+  onNoti?: () => void // return undefined
 }
 
-function Button({ isDisabled = false, children }: ButtonProps) {
+{/* 기본값 설정 가능 ex - button*/}
+function Button({ type = 'button', isDisabled = false, onNoti, children }: ButtonProps) {
   // 구조 분해 할당 (Props는 객체)
   // const { children } = props
   // const isDisabled = false
@@ -17,15 +20,23 @@ function Button({ isDisabled = false, children }: ButtonProps) {
    * - 속성 구조 분해 할당
    */
 
-  let handleClick
+  // if문으로 축약
+  // let handleClick
 
-  if(!isDisabled) {
-    handleClick = () => alert('모든 사용자가 행복해요!!! 🌈')
+  // if(!isDisabled) {
+  //   handleClick = () => alert('모든 사용자가 행복해요!!! 🌈')
+  // }
+
+  // 함수를 전달해서 부모에서 함수를 전달해 실행하는 방법
+  const handleClick = () => {
+    if (isDisabled) return
+    // 옵셔널 체이닝 (함수타입이야? 함수타입이면 실행)
+    onNoti?.()
   }
 
   return (
     <button
-      type="button"
+      type={type}
       className={S.button}
       // 함수가 조금 지저분함
       // onClick={ isDisabled ? undefined : () => alert('모든 사용자가 행복해요!!! 🌈')}
