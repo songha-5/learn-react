4f030a8f916d32491203d3977e0ef8d2c46ba708
songha-5 <shn0518@gmail.com>
2026-03-09 09:57:45 +0900

    학습: 데이터를 UI에 넣기

--- 02-State-이벤트-핸들링/src/app/index.tsx
+++ 02-State-이벤트-핸들링/src/app/index.tsx
@@ -1,5 +1,12 @@
+import Footer from '@/components/Footer'
 import S from './style.module.css'
 
 export default function App() {
-  return <div className={S.container}></div>
+  return (
+    <div className={S.container}>
+      <h1>타이틀</h1>
+
+      <Footer />
+    </div>
+  )
 }
--- 02-State-이벤트-핸들링/src/components/Footer/index.tsx
+++ 02-State-이벤트-핸들링/src/components/Footer/index.tsx
@@ -2,11 +2,13 @@ import S from './style.module.css'
 
 export default function Footer() {
   /**
-   * 데이터 바인딩 (Data Binding)
-   * - JavaScript 데이터(변수, 계산된 값 등)
-   * - JSX 구문 안에 데이터 끼워넣기(Interpolation)
+   * 데이터 바인딩 (Data Binding = Data + JSX = Markup)
+   * - JavaScript 데이터(변수, 상수, 계산된 값(표현식, 함수 반환 값) 등)
+   * - JSX 구문 안에 데이터 끼워넣기(Interpolation {data})
    */
-  const currentYear = 2026
+  // 숫자값이 아닌 date객체를 이용해 현재 값을 가져올 수 있을까?
+  // const currentYear = 2026 
+  const currentYear = new Date().getFullYear() 
 
   /**
    * Props로 데이터 전달(부모 → 자식)
@@ -20,12 +22,13 @@ export default function Footer() {
    * 접근성 고려
    * - 사용자가 이해하기 쉽고 읽기 쉽도록 저작권 레이블(label) 구성
    */
-  const copyrightLabel = '모든 저작권은 이듬(EUID)에 있습니다. 완벽보다 완주를!'
+  const copyrightLabel = `모든 저작권은 이듬(EUID)에 있습니다. ${slogan}`
 
   return (
     <footer className={S.footer}>
-      <small>
-        COPYRIGHT RESERVED. © <abbr title="이듬(EUID)">EUID</abbr>.
+      <small aria-label={copyrightLabel}>
+        {currentYear} COPYRIGHT RESERVED. © <abbr title="이듬(EUID)">EUID</abbr>
+        . {slogan}
       </small>
     </footer>
   )
