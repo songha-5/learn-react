826d064baf47bd264b4730e18c9a198bc4dc75cd
songha-5 <shn0518@gmail.com>
2026-03-09 15:06:09 +0900

    학습: if문으로 랜더링 하기

--- 02-State-이벤트-핸들링/src/learns/ConditionalRendering/ProfileCard/index.tsx
+++ 02-State-이벤트-핸들링/src/learns/ConditionalRendering/ProfileCard/index.tsx
@@ -36,3 +36,39 @@ function ProfileCard({name, role, isNew = false, isBest = false}: PropfileCradPr
 }
 
 export default ProfileCard
+
+// 조건문을 사용하기
+// function ProfileCard({name, role, isNew = false, isBest = false}: PropfileCradProps) {
+
+//   // 조건문은 함수 몸체 안에서 사용
+//   let newBadge = null
+//   if(isNew) {
+//     newBadge = (
+//       <span className={`${S.badge} ${S.badgeNew}`} aria-label="신규 입사자">
+//         NEW
+//       </span>
+//     )
+//   }
+
+//   let bestBadge = null
+//   if(isBest) {
+//     bestBadge = (
+//       <span className={`${S.badge} ${S.badgeBest}`} aria-label="우수 사원">
+//         BEST
+//       </span>
+//     )
+//   }
+//   // JSX안에서 문을 못사용
+//   return (
+//     <article className={S.card}>
+//       <header className={S.header}>
+//         <h3 className={S.name}>{name}</h3>
+//         {newBadge}
+//         {bestBadge}
+//       </header>
+//       <p className={S.role}>{role}</p>
+//     </article>
+//   )
+// }
+
+// export default ProfileCard
