9b54e7671385df8317661ae067635fdb491adb33
songha-5 <shn0518@gmail.com>
2026-03-09 13:45:16 +0900

    학습: 컴포넌트 설계 실습

--- 02-State-이벤트-핸들링/src/app/index.tsx
+++ 02-State-이벤트-핸들링/src/app/index.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import S from './style.module.css'
+import Section from '@/components/Section'
 // import { createElement } from 'react'
 
 export default function App() {
@@ -9,6 +10,10 @@ export default function App() {
       <Header>
         <h1>헤더 컴포넌트</h1>
       </Header>
+
+      <Section title="타이틀입니다" isShowTitle={true}>
+        <p>섹션입니다.</p>
+      </Section>
       <Footer slogan={'완주 이후엔 스스로 학습이 가능!'}/>
       
       {/* 위와 동일한 컴포넌트 */}
--- 02-State-이벤트-핸들링/src/components/Section/index.tsx
+++ 02-State-이벤트-핸들링/src/components/Section/index.tsx
@@ -1,16 +1,40 @@
+import type { ReactNode } from 'react'
 import S from './style.module.css'
 
-function Section() {
+interface SectionProps {
+  title: string
+  isShowTitle: boolean
+  children?: ReactNode
+}
+
+// 조건부 렌더링 문서
+// https://ko.react.dev/learn#conditional-rendering
+
+// 삼항연산식은 JSX에서 쓸수있지만 if문은 밖에서 써서 넣어야함
+
+
+function Section(props: SectionProps) {
+  // if문
+  // let headingClassname 
+  
+  // if(props.isShowTitle) {
+  //   headingClassname = 'sr-only'
+  // }
+
   /**
    * 컴포넌트 title, children 속성
-   * - 컴포넌트 타입 정의 (인라인 → 인터페이스)
-   * - 섹션 제목 설정 (필수)
+   * - 컴포넌트 타입 정의 (인라인 → 인터페이스) ✔
+   * - 섹션 제목 설정 (필수) ✔
    * - 섹션 제목 화면에 표시 여부 설정 (선택)
-   * - 섹션 내부에 넣을 (리액트) 자식 요소 설정 (선택)
+   * - 섹션 내부에 넣을 (리액트) 자식 요소 설정 (선택) ✔
    */
   return (
     <section className={S.container}>
-      <h1 className="sr-only">리액트 학습</h1>
+      {/* 삼항연산문 */}
+      <h1 className={props.isShowTitle ? 'sr-only' : undefined }>{props.title}</h1>
+      {/* if문 */}
+      {/* <h1 className={headingClassname}>{props.title}</h1>  */}
+      {props.children}
     </section>
   )
 }
