218cda74ac2ee62d5a4896bb6e84db705fec323f
songha-5 <shn0518@gmail.com>
2026-03-12 11:16:38 +0900

    학습: 상위부모에서 하위자식에 key로 상태제어 실습 / 개발자도구 설치 및 인터페이스 소개

--- 03-렌더링-원리-불변성/src/app/index.tsx
+++ 03-렌더링-원리-불변성/src/app/index.tsx
@@ -1,10 +1,11 @@
 import { Immutability, ImmutabilityCRUD, Reconciliation, RenderPipeline, VirtualDOM } from '@/learns'
 import S from './style.module.css'
-import { StateInitialization } from '@/practices'
+import { Debugging, StateInitialization } from '@/practices'
 
 export default function App() {
   return (
     <div className={S.container}>
+      <Debugging />
       <StateInitialization />
       <Reconciliation />
       <ImmutabilityCRUD />
--- 03-렌더링-원리-불변성/src/practices/Debugging/index.tsx
+++ 03-렌더링-원리-불변성/src/practices/Debugging/index.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react'
+import UserForm from './UserForm'
 import S from './style.module.css'
 
 export default function Debugging() {
+  const [resetKey, setResetKey] = useState(0)
+  const [message, setMassage] = useState('')
   return (
     <section className={S.container}>
       <h2 className={S.title}>디버깅 (Debugging)</h2>
+
+      <div className={S.inputGroup}>
+        <label htmlFor="debugging-input">메시지</label>
+        <input 
+          id='debugging-input'
+          type="text" 
+          value={message}
+          onChange={(e) => setMassage(e.target.value.trim())}
+        />
+      </div>
+
+      <button 
+        type='button'
+        onClick={() => setResetKey((prve) => prve + 1)}
+      >
+        폼 초기화
+      </button>
+      <UserForm key={resetKey}/>
     </section>
   )
 }
