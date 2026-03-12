f2790ba23682bcedd88a8d26d22c6ef711d1115b
songha-5 <shn0518@gmail.com>
2026-03-12 16:18:35 +0900

    학습: SPA규칙에 맞게 type, json, 자식컴포넌트, css 분리하기

--- 04-리스트-렌더링/src/app/index.tsx
+++ 04-리스트-렌더링/src/app/index.tsx
@@ -1,11 +1,12 @@
 import FruitList from '@/components/SimpleList'
 import S from './style.module.css'
 import StaffList from '@/components/StaffListWithFilter'
-import { ScheduleTable, ShiftManager } from '@/components'
+import { ProductList, ScheduleTable, ShiftManager } from '@/components'
 
 export default function App() {
   return (
     <div className={S.container}>
+      <ProductList />
       <ScheduleTable />
       <ShiftManager />
       <StaffList />
