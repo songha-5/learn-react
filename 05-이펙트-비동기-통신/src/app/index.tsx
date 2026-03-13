1e15489db25cbbe99e24f5a7cca5c1c0ed138e50
songha-5 <shn0518@gmail.com>
2026-03-13 15:26:40 +0900

    학습 : effect기초

--- 05-이펙트-비동기-통신/src/app/index.tsx
+++ 05-이펙트-비동기-통신/src/app/index.tsx
@@ -1,10 +1,10 @@
-import { TodoSearch } from '@/practices'
 import S from './style.module.css'
+import { EffectBasic } from '@/components'
 
 export default function App() {
   return (
     <div className={S.container}>
-      <TodoSearch />
+      <EffectBasic />
     </div>
   )
 }
--- 05-이펙트-비동기-통신/src/components/EffectBasic/EffectBasic.tsx
+++ 05-이펙트-비동기-통신/src/components/EffectBasic/EffectBasic.tsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from 'react'
 import styles from './EffectBasic.module.css'
 
+async function wait(delay = 1000) {
+  return new Promise((resolve) => setTimeout(resolve, delay))
+}
+
 export default function EffectBasic() {
+  const [count, setCount] = useState(0)
+
+  // 이벤트 - 인터렉션
+  const handleIncreaseCount = async () => {
+    await wait(1500)
+    setCount((prev) => prev + 1)
+  }
+  // 이펙트 - 라이프사이클
+  useEffect(() => {
+    document.title = `현재 카운트: ${count}`
+  })
+  
   return (
     <section className={styles.container}>
+      
+
       <h2 className={styles.title}>Effect 기초 학습</h2>
 
-      <button type="button" className={styles.counterButton}>
-        카운트 증가: 0
+      <button type="button" className={styles.counterButton} onClick={handleIncreaseCount}>
+        카운트 증가: {count}
       </button>
 
       <p className={styles.statusText}>
