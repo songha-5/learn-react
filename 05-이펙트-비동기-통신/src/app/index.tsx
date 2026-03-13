e92cb2ba073d80af5608e742907632aec4e03678
songha-5 <shn0518@gmail.com>
2026-03-13 14:05:06 +0900

    학습: 파일 추가

---
+++ 05-이펙트-비동기-통신/src/app/index.tsx
@@ -0,0 +1,10 @@
+import { TodoSearch } from '@/practices'
+import S from './style.module.css'
+
+export default function App() {
+  return (
+    <div className={S.container}>
+      <TodoSearch />
+    </div>
+  )
+}
