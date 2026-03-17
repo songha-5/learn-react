414817850ba9542b1c9a89d4b3a89a63c733ac46
songha-5 <shn0518@gmail.com>
2026-03-17 11:43:16 +0900

    학습: 사용자 ID로 할일 검색 , (데이터 패칭, 상태선언, 조건부 렌더링-로딩, 데이터없음)

--- 05-이펙트-비동기-통신/src/api/getTodo.ts
+++ 05-이펙트-비동기-통신/src/api/getTodo.ts
@@ -1,7 +1,7 @@
 
 export interface Todo {
   message: string
-  todos: []
+  todos: Todo[]
 }
 
 interface GetTodo {
--- 05-이펙트-비동기-통신/src/practices/TodoSearch/TodoSearch.tsx
+++ 05-이펙트-비동기-통신/src/practices/TodoSearch/TodoSearch.tsx
@@ -1,14 +1,52 @@
 import { useEffect, useState } from 'react'
 import S from './TodoSearch.module.css'
 
+// API 엔드포인트(Endpoint)
+const { VITE_API_URL: API_URL } = import.meta.env
+console.log(`${API_URL}/api/todos?userId=${1}`)
+
+// 응답 데이터 타입 지정
+interface ResponseTodosData {
+  message: string
+  todos: Todo[]
+}
+
+interface Todo {
+  id: number
+  content: string
+  completed: boolean
+  userId: number
+}
+
 
 export default function TodoSearch() {
-  const [useId, setUseId] = useState(0)
-  const [isLoading, setIsLoading] = useState(false)
-  const [todos, setTodos] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [userId, setUserId] = useState('')
 
-  // useEffect((
-  // ), [useId])
+  // 사용자 ID값이 변경될때마다 실행
+  useEffect(() => {
+
+    // userId가 빈값이면 함수종료
+    const getTodos = async () => {
+      if (userId === '') {
+        setTodos([])
+        return
+      }
+
+      // 로딩 상태 업데이트
+      setLoading(true)
+
+      // 데이터 요청/응답 (비동기)
+      const response = await fetch(`${API_URL}/api/todos?userId=${userId}`)
+      const data: ResponseTodosData = await response.json()
+      setTodos(data.todos)
+      // 로딩 상태 업데이트
+      setLoading(false)
+    }
+
+    getTodos()
+  }, [userId])
 
   return (
     <section className={S.container}>
@@ -24,6 +62,8 @@ export default function TodoSearch() {
         <input
           id="user-id-input"
           type="number"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value.trim())}
           min={1}
           max={20}
           placeholder="사용자 ID를 입력하세요 (예: 1 ~ 20)"
@@ -31,32 +71,41 @@ export default function TodoSearch() {
       </div>
 
       {/* 할 일 목록 템플릿 */}
-      <ul className={S.list}>
-        <li className={S.item}>
-          <span className={S.textContent}>
-            아직 완료 못한 일
-          </span>
-          <span aria-label="예정" style={{ opacity: 0.3 }}>
-            ❎
-          </span>
-        </li>
-        <li className={S.item}>
-          <span className={`${S.textContent} ${S.completed}`}>
-            완료한 일
-          </span>
-          <span aria-label="완료" style={{ opacity: 1 }}>
-            ✅
-          </span>
-        </li>
-      </ul>
+      {!loading && todos.length > 0 && (
+        <ul className={S.list}>
+          {todos.map(({ id, content/* , completed 실제 데이터 */ }) => {
+            const isDone = getRandomDone() // 할 일 완료 상태 확인 (목적)
+            const doneClassname = isDone ? S.completed : ''
+
+            return (
+              <li key={id} className={S.item}>
+                <span className={`${S.textContent} ${doneClassname}`}>{content}</span>
+                <span
+                  aria-label={isDone ? '완료' : '예정'}
+                  style={{ opacity: isDone ? 1 : 0.3 }}
+                >
+                  {isDone ? '✅' : '❎'}
+                </span>
+              </li>
+            )
+          })}
+        </ul>
+      )}
 
       {/* 상태 알림 템플릿 */}
       <div role="status" className={S.statusRegion}>
         {/* 데이로 로딩 중 표시 메시지 */}
-        <p className={S.loading}>데이터를 가져오고 있습니다...</p>
+        {loading && (
+          <p className={S.loading}>데이터를 가져오고 있습니다...</p>
+        )}
         {/* 검색 결과가 없을 경우 표시 메시지 */}
-        <p>검색 결과가 없습니다.</p>
+        {!loading && userId && todos.length === 0 && (
+          <p>검색 결과가 없습니다.</p>
+        )}
       </div>
     </section>
   )
 }
+
+// 유틸리티 함수
+const getRandomDone = () => Math.random() >= 0.5
