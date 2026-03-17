f9b3e0e982ec3040d0eb26a571586b9dc1b188a0
songha-5 <shn0518@gmail.com>
2026-03-17 15:37:44 +0900

    학습: 닉네임 검사 보완, blur이벤트 추가 compositionEnd이벤트 추가,  isComposing제거

--- 06-폼-핸들링-DOM-참조/src/components/SmartForm/parts/NicknameField.tsx
+++ 06-폼-핸들링-DOM-참조/src/components/SmartForm/parts/NicknameField.tsx
@@ -2,7 +2,7 @@ import { useId } from 'react'
 import S from '../SmartForm.module.css'
 
 const MAX_NICKNAME = 10
-const PROFANITY_PATTERN = '바보 멍청이 또라이'.split(' ').join('|')
+const PROFANITY_PATTERN = '바보 멍청이 ***'.split(' ').join('|')
 const PROFANITY_SUBSTITUTION = '???'
 
 interface Props {
@@ -10,51 +10,44 @@ interface Props {
   onChange: React.Dispatch<React.SetStateAction<string>>
 }
 
-export default function NicknameField({value, onChange}: Props) {
+export default function NicknameField({ value, onChange }: Props) {
   const fieldId = useId()
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
-    const nativeEvent = e.nativeEvent as InputEvent 
-    // TODO 1: [글자 수 제한] 
+
+    // TODO 1: [글자 수 제한]
     // 입력값이 MAX_NICKNAME을 넘으면 잘라내고 리턴하세요.
-    if(value.length > MAX_NICKNAME) {
-      const trancatedValue = value.slice(0, MAX_NICKNAME)
-      onChange(trancatedValue)  
-      return
+    if (value.length > MAX_NICKNAME) {
+      const truncatedValue = value.slice(0, MAX_NICKNAME)
+      onChange(truncatedValue)
+      return // 함수 종료
     }
 
-    // TODO 2: [조합 중(IME) 처리]
-    // 한글이 조립 중(isComposing)일 때는 필터링을 건너뛰고 상태만 업데이트하세요.
-    if(nativeEvent.isComposing) {
-      //비속어 필터링 기능은 한글 조립중에 작동될 경우 이상하게 작동될 것이므로 조립중에는 상태 업데이트만 수행
-      onChange(value)
-      return
-    }
-
-    // TODO 3: [영문/숫자/조합 완료 시 필터링]
-    // replace와 정규식을 사용해 비속어를 '??'로 치환하세요.
-    onChange(e.currentTarget.value)
+    onChange(value)
   }
-
-  const handleCompositionEnd = (e: React.CompositionEvent<HTMLInputElement>) => {
-    console.log('한글 조합 완료')
-    // TODO 4: [최종 확정 필터링]
+  
+  const changeProfanity = (value: string) => {
+    // TODO 2: [영문/숫자/조합 완료 시 필터링]
+    // String.prototype.replace와 정규식(Regular Expression, RegExp)을 사용해
     // 한글 조합이 완전히 끝나는 시점에 다시 한번 비속어를 걸러주세요.
-    compositionIME(e.currentTarget.value)
+    const filteredValue = value.replace(
+      new RegExp(PROFANITY_PATTERN, 'g'), // /바보|멍청이|***/g
+      PROFANITY_SUBSTITUTION, // '???'
+    )
+  
+    onChange(filteredValue) // 닉네임 상태 업데이트 요청 (다음번 렌더링에서 반영)
   }
-
-  const compositionIME = (value: string) => {
-    const filteredValue = value.replace(new RegExp(PROFANITY_PATTERN, 'i'), PROFANITY_SUBSTITUTION)
-    onChange(filteredValue)
-  }
-
 
   return (
     <div className={S.field}>
       <div className={S.labelWrapper}>
-        <label htmlFor={fieldId} className={S.label}>닉네임</label>
-        <span className={S.counter}>{value.length}/{MAX_NICKNAME}</span>
+        <label htmlFor={fieldId} className={S.label}>
+          닉네임
+        </label>
+        <span className={S.counter}>
+          {value.length}/{MAX_NICKNAME}
+        </span>
       </div>
       <input
         id={fieldId}
@@ -62,7 +55,8 @@ export default function NicknameField({value, onChange}: Props) {
         placeholder="닉네임을 입력하세요"
         value={value}
         onChange={handleChange}
-        onCompositionEnd={handleCompositionEnd}
+        onCompositionEnd={(e) => changeProfanity(e.currentTarget.value)}
+        onBlur={(e) => changeProfanity(e.currentTarget.value)}
       />
     </div>
   )
