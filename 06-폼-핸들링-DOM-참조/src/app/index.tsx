ce6e4fdff8ee4e92432877b73640ff120efead9a
songha-5 <shn0518@gmail.com>
2026-03-18 16:56:07 +0900

    학습 : useRef 준비

--- 06-폼-핸들링-DOM-참조/src/app/index.tsx
+++ 06-폼-핸들링-DOM-참조/src/app/index.tsx
@@ -1,9 +1,10 @@
-import { FormSubmission, MultiInputForm, SmartForm } from '@/components'
+import { FormSubmission, MultiInputForm, RefStudy, SmartForm } from '@/components'
 import S from './style.module.css'
 
 export default function App() {
   return (
     <div className={S.container}>
+      <RefStudy />
       <FormSubmission />
       <MultiInputForm />
       <SmartForm />
