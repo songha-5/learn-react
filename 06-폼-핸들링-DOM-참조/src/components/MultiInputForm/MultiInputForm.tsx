4f94b4645331034aef8084b9aecb31a89aa2ebd0
songha-5 <shn0518@gmail.com>
2026-03-18 13:48:34 +0900

    학습: 타입 알리아스

--- 06-폼-핸들링-DOM-참조/src/components/MultiInputForm/MultiInputForm.tsx
+++ 06-폼-핸들링-DOM-참조/src/components/MultiInputForm/MultiInputForm.tsx
@@ -1,4 +1,4 @@
-import { useId } from 'react'
+import { useId, useState } from 'react'
 import NicknameField from './parts/NicknameField'
 import EmailField from './parts/EmailField'
 import PasswordField from './parts/PasswordField'
@@ -8,19 +8,35 @@ import S from './MultiInputForm.module.css'
 // -------------------------------------------------------------------
 // 실습 가이드
 // -------------------------------------------------------------------
-// 1. 모든 필드의 초기값을 가진 객체 'INITIAL_STATE'를 컴포넌트 외부에 정의하세요.
-// 2. INITIAL_STATE를 사용하는 하나의 'formState' 상태(객체형)를 선언합니다.
+// 1. 모든 필드의 초기값을 가진 객체 'INITIAL_FORM_STATE'를 컴포넌트 외부에 정의하세요.
+// 2. INITIAL_FORM_STATE를 사용하는 하나의 'formState' 상태(객체형)를 선언합니다.
 // 3. name과 value를 인자로 받아 formState를 업데이트하는 함수를 작성하세요.
 // 4. 폼 초기화(reset) 이벤트 핸들러를 작성하세요.
 // -------------------------------------------------------------------
 
+// 하나의 객체로 묶어서 관리
+const INITIAL_FORM_STATE= {
+  nickname : '',
+  email : '',
+  password : '',
+  passwordConfirm : '',
+}
+
+// 사용자 정의 타입 알리아스로 정의하면 쉽게 할 수 있음
+// interface FormState {
+//   nickname: string
+//   email: string
+//   password: string
+//   passwordConfirm: string
+// }
+
+// 사용자 정의 타입 알리아스 사용
+type FormState = typeof INITIAL_FORM_STATE
+
 export default function MultiInputForm() {
   const sectionId = useId()
+  const [formState] = useState<FormState>(INITIAL_FORM_STATE)
 
-  const nickname = ''
-  const email = ''
-  const password = ''
-  const passwordConfirm = ''
 
   return (
     <article className={S.card} aria-labelledby={sectionId}>
@@ -35,20 +51,20 @@ export default function MultiInputForm() {
 
       <form className={S.form}>
         <NicknameField
-          value={nickname}
+          value={formState.nickname}
           onChange={() => {}}
         />
         <EmailField
-          value={email}
+          value={formState.email}
           onChange={() => {}}
         />
         <PasswordField
-          value={password}
+          value={formState.password}
           onChange={() => {}}
         />
         <PasswordConfirmField
-          value={passwordConfirm}
-          basePassword={password}
+          value={formState.passwordConfirm}
+          basePassword={formState.password}
           onChange={() => {}}
         />
         <div role="group" className={S.buttonGroup}>
