9aa4fbd70e61dcf1d945ca9ce22bd2d80376e15f
songha-5 <shn0518@gmail.com>
2026-03-20 13:09:45 +0900

    학습: 뮤테이션을 하면 안되는 이유

--- 07-복잡한-상태-Immer/src/components/ReferenceCheck/ReferenceCheck.tsx
+++ 07-복잡한-상태-Immer/src/components/ReferenceCheck/ReferenceCheck.tsx
@@ -1,28 +1,73 @@
+import { useState } from 'react'
 import S from './ReferenceCheck.module.css'
 
 export default function ReferenceCheck() {
-  
-  const user = {
+
+  // 셋다 동일
+  const INITAL_PROFILE = {
+    city: '서울',
+    postcode: '013456'
+  }
+
+  const INITAL_USER = {
     name: '이두나',
     age: 21,
     profile: {
-      city: '서울',
+      ...INITAL_PROFILE
     },
   }
+  // const INITAL_USER = {
+  //   name: '이두나',
+  //   age: 21,
+  //   profile: {
+  //     city: '서울',
+  //     postcode: '013456'
+  //   },
+  // }
 
+  type User = typeof INITAL_USER
+
+  const [user, setUser] = useState<User>({
+    ...INITAL_USER,
+    profile: {
+      ...INITAL_USER.profile
+    }
+  })
+
+  // 원본데이터를 엎어버림으로 예기치 못한 오류 발생
   const handleWrongUpdate = () => {
+
+    user.name = '강하영'
+    user.profile.postcode = '91920'
+    
+   const nextUser = user
+   setUser(nextUser)
+
+
     // ❌ Mutation: 데이터는 바뀌지만 참조가 같음
     console.error('뮤테이션')
+    console.table(nextUser)
   }
-
+  // 참조해서 수정해야 예기치 못한 오류 X
   const handleRightUpdate = () => {
+     const nextUser: User = {
+      ...user,
+      name:'강하영',
+      profile: {
+        ...user.profile,
+        city: '이천'
+      }
+    }
+    setUser(nextUser)
     // ✅ Immutability: 새로운 객체 생성 (새로운 참조 주소)
     console.log('%c✅ 불변성 유지:', 'color: #04a200;')
+    console.table(nextUser)
   }
 
   const handleReset = () => {
     // ⚠️ Mutation: 예상치 못한 결과 확인(참조형 데이터 오염)
     console.warn('참조형 데이터 오염')
+    setUser(INITAL_USER)
   }
 
   return (
@@ -48,6 +93,10 @@ export default function ReferenceCheck() {
             <div className={S.infoRow}>
               <dt className={S.label}>도시</dt>
               <dd className={S.badge}>{user.profile.city}</dd>
+            </div>
+            <div className={S.infoRow}>
+              <dt className={S.label}>우편번호</dt>
+              <dd className={S.badge}>{user.profile.postcode}</dd>
             </div>
           </dl>
         </div>
