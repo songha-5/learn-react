223ae33b4bf6c68c3dbbf0a07e7a8d29152770b3
songha-5 <shn0518@gmail.com>
2026-03-23 10:36:12 +0900

    학습: useImmer 적용 / 파생된상태 활용 / 불필요한 useEffect 걷어내기

--- 07-복잡한-상태-Immer/src/app/index.tsx
+++ 07-복잡한-상태-Immer/src/app/index.tsx
@@ -1,9 +1,10 @@
-import { ReferenceCheck, TodosCRUD } from '@/components'
+import { DerivedState, ReferenceCheck, TodosCRUD } from '@/components'
 import S from './style.module.css'
 
 export default function App() {
   return (
     <div className={S.container}>
+      <DerivedState />
       <TodosCRUD />
       <ReferenceCheck />
     </div>
--- 07-복잡한-상태-Immer/src/components/DerivedState/DerivedState.tsx
+++ 07-복잡한-상태-Immer/src/components/DerivedState/DerivedState.tsx
@@ -1,57 +1,51 @@
 /* eslint-disable react-hooks/set-state-in-effect */
 
+import { useImmer } from 'use-immer'
 import { useEffect, useState } from 'react'
 import { INITIAL_PRODUCTS } from './data/products'
 import S from './DerivedState.module.css'
 
 export default function DerivedState() {
   // Immer 라이브러리를 사용해 상태를 관리해보세요.
-  const [products, setProducts] = useState(INITIAL_PRODUCTS)
+  const [products, setProducts] = useImmer(INITIAL_PRODUCTS)
 
   // 총 상품 가격 상태 ------------------------------------------------------
 
-  const [totalPrice, setTotalPrice] = useState(() => {
-    const totalPrice = products.reduce(
-      (sum, { price, quantity }) => sum + price * quantity,
-      0,
-    )
+  /**
+ * -------------------------------------------------
+ * 상태(State)인지 아닌지 판단하는 3단계 질문
+ * -------------------------------------------------
+ * 1. 부모로부터 Props로 전달되는 데이터인가요?
+ * 2. 시간이 지나도 변경되지 않는 데이터인가요?
+ * 3. 다른 State나 Prop으로 계산 가능한가요?
+ *
+ *    그렇다면 관리할 상태(State)가 아닙니다.
+ */
 
-    return totalPrice
-  })
-
-  useEffect(() => {
-    const nextTotalPrice = products.reduce(
-      (total, product) => total + product.price * product.quantity,
-      0,
-    )
-
-    // ⚠️ 잠깐!
-    // 특정 상품의 수량이 3개를 넘어가면 특별 할인이 적용될 예정이라
-    // 아직 로직이 확정 안 됐으니 업데이트를 잠시 막아두자! 라고
-    // 잘못 판단(실수)할 경우 작동에 문제가 발생할 수 있어요.
-    const [macbook] = products
-    if (macbook && macbook.quantity <= 3) {
-      setTotalPrice(nextTotalPrice)
-    }
-  }, [products])
+  // useEffect로 불필요하게 중복 실행되는 코드를
+  // 분리함 ( 두번 렌더링 되고있었고 오류 야기 )
+  const totalPrice = products.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0,
+  )
 
   // 총 개수 상태 ----------------------------------------------------------
 
-  const [totalCount, setTotalCount] = useState(() => products.length)
-
-  useEffect(() => {
-    // ⚠️ 잠깐!
-    // 상품(product)의 '수량'이 변경된 것이지, '품목 개수'가 변경된 게 아닌데도
-    // 매번 이펙트 함수가 실행되고 setTotalCount(리렌더링 유발)를 호출하고 있습니다.
-    // 이는 명백한 자원 낭비 아닐까요?
-    const totalCount = products.length
-    setTotalCount(totalCount)
-  }, [products])
+  // useEffect로 불필요하게 중복 실행되는 코드를
+  // 분리함 ( 두번 렌더링 되고있었고 오류 야기 )
+  const totalCount = products.length
 
   // 수량 업데이트 함수 ------------------------------------------------------
 
   const updateQuantity = (id: string, delta: number) => {
-    const nextProducts = products.map((product) => {
+    setProducts((draft) => {
+      const product = draft.find(item => item.id === id)
+      if(product) {
+        if (!product.options.inStock) return
+        product.quantity = Math.max(1, product.quantity + delta)
+      }
+    })
+    /* const nextProducts = products.map((product) => {
       if (product.id !== id || !product.options.inStock) return product
 
       const nextProduct = {
@@ -62,7 +56,7 @@ export default function DerivedState() {
       return nextProduct
     })
 
-    setProducts(nextProducts)
+    setProducts(nextProducts) */
   }
 
   return (
@@ -73,6 +67,7 @@ export default function DerivedState() {
         {products.map((product) => {
           const { id, name, price, options, quantity } = product
           const hasInStock = options.inStock
+          const isDisabled = quantity === 1
           const currency = price.toLocaleString()
 
           const productLabel = `${name} 재고 ${hasInStock ? '있음' : '없음'} (${currency}원)`
@@ -91,7 +86,7 @@ export default function DerivedState() {
                   type="button"
                   className={S.button}
                   onClick={() => updateQuantity(id, -1)}
-                  aria-disabled={!hasInStock}
+                  aria-disabled={!hasInStock || isDisabled}
                   aria-label={`${name} 수량 감소`}
                 >
                   -
