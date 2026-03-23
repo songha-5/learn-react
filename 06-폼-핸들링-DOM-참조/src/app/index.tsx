3816c351c7ff9961bc70088b6e0561be3f65450f
songha-5 <shn0518@gmail.com>
2026-03-23 09:14:53 +0900

    학습 파일추가

--- 06-폼-핸들링-DOM-참조/src/app/index.tsx
+++ 06-폼-핸들링-DOM-참조/src/app/index.tsx
@@ -1,10 +1,12 @@
 import { FileUpload, FormSubmission, MultiInputForm, RefStudy, SmartForm } from '@/components'
 import S from './style.module.css'
 import { SignUpForm } from '@/practices'
+import TodosCrudWithImmer from '@/components/TodosCrudWithImmer/TodosCrudWithImmer'
 
 export default function App() {
   return (
     <div className={S.container}>
+      <TodosCrudWithImmer />
       <SignUpForm />
       {/* <FileUpload />
       <RefStudy />
