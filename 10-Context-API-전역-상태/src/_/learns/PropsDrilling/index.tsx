af93ea8ebb0027c5611a0747d704e2a67878aa5b
songha-5 <shn0518@gmail.com>
2026-03-26 09:04:38 +0900

    학습: 파일추가

---
+++ 10-Context-API-전역-상태/src/_/learns/PropsDrilling/index.tsx
@@ -0,0 +1,12 @@
+import GrandFather from './parts/GrandFather'
+import S from './style.module.css'
+
+export default function PropsDrilling() {
+
+  return (
+    <section className={`${S.box} ${S.container}`}>
+      <h1 className={S.title}>깊숙히 컴포넌트 Props 전달</h1>
+      <GrandFather />
+    </section>
+  )
+}
