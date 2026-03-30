a9f5a265fbfe0e54e1fc646cf8ec41ab21399ff9
songha-5 <shn0518@gmail.com>
2026-03-30 17:02:44 +0900

    학습: 파일변경

--- 11-싱글-페이지-애플리케이션/02-pokedot-app/src/app/index.tsx
+++ 11-싱글-페이지-애플리케이션/02-pokedot-app/src/app/index.tsx
@@ -1,32 +1,28 @@
-import { AuthProvider } from '@/contexts/AuthContext'
-import { ThemeProvider } from '@/contexts/ThemeContext'
-import { ModalProvider } from '@/contexts/ModalContext'
-import { CollectionProvider } from '@/contexts/CollectionContext'
 import S from './style.module.css'
 import '@/styles/main.css'
+import AppProvider from '@/contexts/AppContext'
+import { Navbar } from '@/components'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from '@/pages/HomePage'
+import { NAVIGATION } from '@/config/paths'
 
 export default function App() {
   return (
     // AppProvider를 사용해 프로바이더 래퍼 헬 문제를 해결해보세요.
-    <AuthProvider>
-      <ThemeProvider>
-        <ModalProvider>
-          <CollectionProvider> {/* 🤯 */}
+    <AppProvider>
 
-            <div className={S.container}>
-              {/* 앱 내비게이션 컴포넌트를 추가합니다.  */}
+      <div className={S.container}>
+        {/* 앱 내비게이션 컴포넌트를 추가합니다.  */}
+        <Navbar />
+        {/* 
+          앱 라우트(routes)를 구성합니다.
+          - src/pages 폴더의 페이지 컴포넌트를 확인하세요.
+          */}
+          <Routes>
+            <Route path={NAVIGATION.home} element={<HomePage />}/>
+          </Routes>
+      </div>
 
-              <main>
-                {/* 
-                  앱 라우트(routes)를 구성합니다.
-                  - src/pages 폴더의 페이지 컴포넌트를 확인하세요.
-                */}
-              </main>
-            </div>
-
-          </CollectionProvider>
-        </ModalProvider>
-      </ThemeProvider>
-    </AuthProvider>
+    </AppProvider>
   )
 }
