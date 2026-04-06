8ab783019a73f4134569083e287237bf6e3889ec
songha-5 <shn0518@gmail.com>
2026-04-06 10:35:53 +0900

    수정: 파일경로 수정

--- next.js/03-파일-기반-라우팅-시스템/02-다이내믹-라우팅/src/app/books/_resources/[title]/page.tsx
+++ next.js/03-파일-기반-라우팅-시스템/02-다이내믹-라우팅/src/app/books/_resources/[title]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation"
-import { Book, books } from "../_resources/data"
+import { Book, books } from "../data"
 
 interface Props {
   params: Promise<{ title: Book['title'] }>
